2468604689646e460550b69399c3d1963ec4bef1
william <william.munozr@gmail.com>
2023-03-20 14:16:45 -0500

    fixing warnings

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -14,7 +15,7 @@ var recipes []Recipe
 func init() {
 	recipes = make([]Recipe, 0)
 	file, _ := os.ReadFile("recipes.json")
-	_ = json.Unmarshal([]byte(file), &recipes)
+	_ = json.Unmarshal(file, &recipes)
 }
 
 type Recipe struct {
@@ -45,9 +46,38 @@ func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
-	router.Run()
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	err := router.Run()
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
 }
