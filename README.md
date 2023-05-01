8a0d30a08fe26a67710de63ae4485829c623242d
william <william.munozr@gmail.com>
2023-05-01 10:44:44 -0500

    add swagger documentation

--- README.md
+++ README.md
@@ -8,6 +8,28 @@
 $ go get github.com/rs/xid
 ```
 
+### Installing Go Swagger
+
+```commandline
+brew tap go-swagger/go-swagger
+brew install go-swagger
+```
+
+#### Validate the Swagger version
+
+```commandline
+swagger version                                                                                              ─╯
+version: v0.30.3
+commit: ecf6f05b6ecc1b1725c8569534f133fa27e9de6b
+```
+
+#### Generate a spec file
+
+```commandline
+swagger generate spec -o ./swagger.json
+```
+
 ## References
 
 - [Globally Unique ID Generator](https://github.com/rs/xid)
+- [Swagger Meta Configuration](https://goswagger.io/use/spec/meta.html)
--- main.go
+++ main.go
@@ -1,3 +1,21 @@
+// Recipes API
+//
+// This is a sample recipes API. You can find out more about the API at https://github.com/williammunozr/recipes-api.
+//
+// Schemes: http
+// Host: localhost:8080
+// BasePath: /
+// Version: 1.0.0
+// Contact: William Munoz
+//
+// <william.munozr@gmail.com> https://godeveloper.io
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+// swagger:meta
 package main
 
 import (
@@ -44,10 +62,39 @@ func NewRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+// swagger:operation GET /recipes recipes listRecipes
+// Returns list of recipes
+// ---
+// produces:
+// - application/json
+// responses:
+//
+//	'200':
+//		description: Successful operation
 func ListRecipesHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+// swagger:operation PUT /recipes/{id} recipes updateRecipe
+// Update an existing recipe
+// ---
+// parameters:
+//   - name: id
+//     in: path
+//     description: ID of the recipe
+//     required: true
+//     type: string
+//
+// produces:
+// - application/json
+// responses:
+//
+//	    	'200':
+//				description: Successful operation
+//			'400':
+//				description: Invalid input
+//			'404':
+//				description: Invalid recipe ID
 func UpdateRecipeHandler(c *gin.Context) {
 	id := c.Param("id")
 	var recipe Recipe
